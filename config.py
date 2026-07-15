aa7ae0c46ac83147f0429e657d0d2851a2adc660
ozkansuleyman89-droid <ozkansuleyman89@gmail.com>
2026-07-16 00:55:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Telegram
+BOT_TOKEN = "123456789:AAxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CHAT_ID = "-1001234567890"
+
+# Filtreler
+FUNDING_LIMIT = -0.005      # -0.50%
+PRICE_CHANGE_LIMIT = 15.0   # %15
+CHECK_INTERVAL = 300         # 5 dakika
