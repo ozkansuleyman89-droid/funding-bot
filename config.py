a6c69e17f499a2f45930adcdf12079a3d1cdd7fe
ozkansuleyman89-droid <ozkansuleyman89@gmail.com>
2026-07-16 00:56:37 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Telegram
-BOT_TOKEN = "123456789:AAxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-CHAT_ID = "-1001234567890"
-
-# Filtreler
-FUNDING_LIMIT = -0.005      # -0.50%
-PRICE_CHANGE_LIMIT = 15.0   # %15
-CHECK_INTERVAL = 300         # 5 dakika
