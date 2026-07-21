6094e0cbf2993a63f65617fd883325c8cd7ac4bd
ozkansuleyman89-droid <ozkansuleyman89@gmail.com>
2026-07-21 23:47:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+print("1")
+
+import json
+print("2")
+
+import websocket
+print("3")
+
+import requests
+print("4")
+
+import time
+print("5")
+
+from config import *
+print("6")
+
+print("BITTI")
+
+while True:
+    time.sleep(10)
