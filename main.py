15ed84ea6706be5fa4667cbece1adb2d24ea4dbf
ozkansuleyman89-droid <ozkansuleyman89@gmail.com>
2026-07-21 23:44:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,84 +1,21 @@
+print("1")
+
 import json
+print("2")
+
 import websocket
+print("3")
+
 import requests
+print("4")
+
 import time
+print("5")
 
 from config import *
+print("6")
 
-TELEGRAM_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+print("BITTI")
 
-
-def telegram(text):
-    try:
-        requests.post(
-            TELEGRAM_URL,
-            data={
-                "chat_id": CHAT_ID,
-                "text": text
-            },
-            timeout=10
-        )
-    except Exception as e:
-        print(e)
-
-
-sent = False
-
-
-def on_open(ws):
-    print("WebSocket Bağlandı")
-    telegram("✅ Bot başladı\nWebSocket bağlandı.")
-
-
-def on_message(ws, message):
-    global sent
-
-    data = json.loads(message)
-
-    if not isinstance(data, list):
-        return
-
-    if len(data) == 0:
-        return
-
-    coin = data[0]
-
-    symbol = coin.get("s")
-    price = coin.get("c")
-
-    print(symbol, price)
-
-    if not sent:
-        telegram(f"📡 İlk veri geldi\n\n{symbol}\nFiyat: {price}")
-        sent = True
-
-
-def on_error(ws, error):
-    print("HATA:", error)
-    telegram(f"❌ WebSocket Hatası\n{error}")
-
-
-def on_close(ws, code, msg):
-    print("Bağlantı kapandı")
-    telegram("⚠️ WebSocket kapandı")
-
-    time.sleep(5)
-
-    start()
-
-
-def start():
-    ws = websocket.WebSocketApp(
-        "wss://fstream.binance.com/ws/!miniTicker@arr",
-        on_open=on_open,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close,
-    )
-
-    ws.run_forever()
-
-
-print("BOT BAŞLIYOR...")
-
-start()
+while True:
+    time.sleep(10)
